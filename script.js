224756f27b731d978756433132b5ca91557e5e40
sanoyon211 <sanoyon522@gmail.com>
2026-02-24 19:35:32 +0600

    get all element by id

---
+++ script.js
@@ -0,0 +1,17 @@
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = 'all';
+
+let cardStatusMap = {};
+
+let total = document.getElementById('total');
+let interviewCount = document.getElementById('interviewCount');
+let rejectedCount = document.getElementById('rejectedCount');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+const allCardSection = document.getElementById('allCards');
+const mainContainer = document.querySelector('main');
+const filterSection = document.getElementById('filtered-section');
